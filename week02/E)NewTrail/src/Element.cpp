b765d786581bfd71788cae5b4b1fb1c07e46c282
Gustavo Faria <gusfaria11@gmail.com>
2013-09-10 11:23:42 -0400

    homework week 2 update

---
+++ week02/E)NewTrail/src/Element.cpp
@@ -0,0 +1,9 @@
+//
+//  Element.cpp
+//  NewTrail
+//
+//  Created by Gustavo Faria on 10/09/13.
+//
+//
+
+#include "Element.h"
---
+++ week02/E)NewTrail/src/Element.h
@@ -0,0 +1,14 @@
+//
+//  Element.h
+//  NewTrail
+//
+//  Created by Gustavo Faria on 10/09/13.
+//
+//
+
+#ifndef __NewTrail__Element__
+#define __NewTrail__Element__
+
+#include <iostream>
+
+#endif /* defined(__NewTrail__Element__) */
