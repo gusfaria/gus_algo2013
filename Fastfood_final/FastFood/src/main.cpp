f6a7fb551f7d7a33a1bb4b6d9f5fbb7b934e67c1
Gustavo Faria <gusfaria11@gmail.com>
2013-12-14 20:25:17 -0500

    push my algo final

---
+++ Fastfood_final/FastFood/src/main.cpp
@@ -0,0 +1,10 @@
+#include "testApp.h"
+#include "ofAppGlutWindow.h"
+
+int main() {
+	ofAppGlutWindow window;
+//	ofSetupOpenGL(&window, 1024, 768, OF_WINDOW);
+    ofSetupOpenGL(&window, 1024, 768, OF_FULLSCREEN);
+
+	ofRunApp(new testApp());
+}
